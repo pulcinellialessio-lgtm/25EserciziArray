19b3aa3fe1afd4852fa43b8160e1e3f496cda4fe
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2025-11-09 10:24:49 +0100

    Aggiungere i file di progetto.

---
+++ 25eserciziArray/Program.cs
@@ -0,0 +1,246 @@
+﻿namespace _25eserciziArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Che esercizio vuoi fare?");
+            int es = Convert.ToInt32(Console.ReadLine());
+
+            if (es == 1)
+            {
+                int[] array1;
+
+                Console.WriteLine("Dimmi la dimensione del vettore");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                array1 = new int[num];
+
+                for (int i = 0; i < num; i++)
+                {
+                    Console.WriteLine("Dimmi il " + i + "° valore");
+                    int numero = Convert.ToInt32(Console.ReadLine());
+
+                    array1[i] = numero;
+                }
+
+                for (int i = num - 1; i >= 0; i--)
+                {
+                    Console.WriteLine("I numeri sono: " + array1[i]);
+                }
+            }
+            else if (es == 2)
+            {
+                int[] array2;
+
+                Console.WriteLine("Dimmi la dimensione del vettore");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                array2 = new int[num1];
+
+                for (int i = 0; i < num1; i++)
+                {
+                    Console.WriteLine("Dimmi il " + i + "° valore");
+                    int numero1 = Convert.ToInt32(Console.ReadLine());
+
+                    array2[i] = numero1;
+                }
+
+                for (int i = 0; i < num1; i++)
+                {
+
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine("I numeri sono: " + array2[i]);
+                    }
+                }
+            }
+            else if (es == 3)
+            {
+                int[] array3;
+
+                Console.WriteLine("Dimmi la dimensione del vettore");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                array3 = new int[num2];
+
+                for (int i = 0; i < num2; i++)
+                {
+                    Console.WriteLine("Dimmi il " + i + "° valore");
+                    int numero2 = Convert.ToInt32(Console.ReadLine());
+
+                    array3[i] = numero2;
+                }
+
+
+                for (int i = 0; i < num2; i++)
+                {
+
+                    if (array3[i] % 2 != 0)
+                    {
+                        Console.WriteLine("I numeri sono: " + array3[i]);
+                    }
+                }
+            }
+            else if (es == 4)
+            {
+                int[] array4;
+
+                Console.WriteLine("Dimmi la dimensione del vettore");
+                int num3 = Convert.ToInt32(Console.ReadLine());
+
+                array4 = new int[num3];
+
+                for (int i = 0; i < num3; i++)
+                {
+                    Console.WriteLine("Dimmi il " + i + "° valore");
+                    int numero3 = Convert.ToInt32(Console.ReadLine());
+
+                    array4[i] = numero3;
+                }
+
+                int p = 0, q = 0;
+
+                for (int i = 0; i < num3; i++)
+                {
+                    if (array4[i] % 2 != 0)
+                    {
+                        p = p + 1;
+                    }
+                    else
+                    {
+                        q = q + 1;
+                    }
+                }
+
+                Console.WriteLine("I numeri pari sono: " + q);
+                Console.WriteLine("I numeri dispari sono: " + p);
+
+            }
+            else if (es == 5)
+            {
+                int[] array5;
+
+                Console.WriteLine("Dimmi la dimensione del vettore");
+                int num4 = Convert.ToInt32(Console.ReadLine());
+
+                array5 = new int[num4];
+
+                for (int i = 0; i < num4; i++)
+                {
+                    Console.WriteLine("Dimmi il " + i + "° valore");
+                    int numero4 = Convert.ToInt32(Console.ReadLine());
+
+                    array5[i] = numero4;
+                }
+
+                int min = array5[0], max = array5[0], somma = 0, l = 0, indiceMin = 1000000, indiceMax = -1000000;
+                float media;
+
+                for (int i = 1; i < num4; i++)
+                {
+                    if (min > array5[i])
+                    {
+                        min = array5[i];
+                        indiceMin = i;
+                    }
+
+                    if (max < array5[i])
+                    {
+                        max = array5[i];
+                        indiceMax = i;
+                    }
+
+                    somma = somma + array5[i];
+
+                    l = l + 1;
+                }
+
+                media = somma / l;
+
+                Console.WriteLine("Il numero minimo è: " + min + " nella posizione " + indiceMin);
+                Console.WriteLine("Il numero massimo è: " + max + " nella posicione " + indiceMax);
+                Console.WriteLine("la media è: " + media);
+                Console.WriteLine("la somma è " + somma);
+            }
+            else if (es == 6)
+            {
+                int[] array6;
+
+                Console.WriteLine("Dimmi la dimensione del vettore");
+                int num5 = Convert.ToInt32(Console.ReadLine());
+
+                array6 = new int[num5];
+
+                for (int i = 0; i < num5; i++)
+                {
+                    Console.WriteLine("Dimmi il " + i + "° valore");
+                    int numero5 = Convert.ToInt32(Console.ReadLine());
+
+                    array6[i] = numero5;
+                }
+
+                Console.WriteLine("dimmi un numero");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                int m = 0, M = 0;
+
+                //conto i valori per determinare la lunghezza dei vettori
+
+                for (int i = 0; i < array6.Length; i++)
+                {
+                    if (array6[i] < x)
+                    {
+
+                        m++;
+                    }
+                    else
+                    {
+
+                        M++;
+                    }
+                }
+                int[] vet1 = new int[m];
+                int[] vet2 = new int[M];
+                int indxvet1 = 0, indxvet2 = 0;
+
+                for (int i = 0; i < array6.Length; i++)
+                {
+                    if (array6[i] < x)
+                    {
+                        vet1[indxvet1] = array6[i];
+                        indxvet1++;
+                    }
+                    else
+                    {
+                        vet2[indxvet2] = array6[i];
+                        indxvet2++;
+                    }
+                }
+
+                for (int i = 0; i < vet1.Length; i++)
+                {
+                    Console.Write($"{vet1[i]}  ");
+                }
+                Console.Write("    ");
+                for (int i = 0; i < vet2.Length; i++)
+                {
+                    Console.Write($"{vet2[i]}  ");
+                }
+
+            }
+            else if (es == 7)
+            {
+                int[] array7 = { 1, 2, 3, 4 };
+                int[] array8 = { 5, 6, 7, 8 };
+
+                int[] vettoreUnione = new int[array7.Length + array8.Length];
+
+                for (int i = 0; i <= array7.Length; i++)
+                {
+
+                }
+            }
+        }
+    }
+}
